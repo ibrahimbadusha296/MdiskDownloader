38f9c1266ac41ea41b7b546a71ed23a95e392bb0
jugnucode <101200766+jugnucode@users.noreply.github.com>
2023-03-05 19:53:06 +0530

    heroku

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ def upstatus(statusfile,message):
         with open(statusfile,"r") as upread:
             txt = upread.read()
         try:
-            app.edit_message_text(message.chat.id, message.id, f"__Uploaded__ : **{txt}**")
+            app.edit_message_text(message.chat.id, message.id, f"Join @cybersrainbot for more bots __Uploaded__ :  **{txt}**")
             time.sleep(10)
         except:
             time.sleep(5)
